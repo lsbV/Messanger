eef78f1ba8d0285352c37f31a9e832fc137bbd79
lsbV <119605214+lsbV@users.noreply.github.com>
2024-12-13 14:55:08 +0200

    Add project files.

---
+++ Core/Message.cs
@@ -0,0 +1,57 @@
+﻿namespace Core;
+
+public record Message(
+    MessageId Id,
+    SenderId SenderId,
+    RecipientId RecipientId,
+    MessageContent Content,
+    MessageStatus Status,
+    DateTime CreatedAt,
+    DateTime? EditedAt = null
+    );
+
+public record MessageId(Guid Value)
+{
+    public static Guid Of(Guid value)
+    {
+        return value;
+    }
+}
+
+
+public record SenderId(Guid Value)
+{
+    public static Guid Of(Guid value)
+    {
+        return value;
+    }
+}
+
+public record RecipientId(Guid Value)
+{
+    public static Guid Of(Guid value)
+    {
+        return value;
+    }
+}
+
+public abstract record MessageContent(TextMessageContent MainContent, AdditionContent? AdditionContent);
+
+public abstract record AdditionContent;
+public abstract record MainContent;
+public record TextMessageContent(string Value) : MainContent;
+
+public record ImageMessageContent(string Url) : AdditionContent;
+
+public record VideoMessageContent(string Url) : AdditionContent;
+
+public record AudioMessageContent(string Url) : AdditionContent;
+
+public record FileMessageContent(string Url) : AdditionContent;
+
+public enum MessageStatus
+{
+    Sent,
+    Delivered,
+    Read
+}
---
+++ Core/User.cs
@@ -0,0 +1,34 @@
+﻿namespace Core;
+
+public record User(UserId Id, UserName Name, Email Email)
+{
+    public static User Create(UserId id, UserName name, Email email)
+    {
+        return new User(id, name, email);
+    }
+}
+
+public record UserId(Guid Value)
+{
+    public static Guid Of(Guid value)
+    {
+        return value;
+    }
+
+}
+
+public record UserName(string Value)
+{
+    public static string Of(string value)
+    {
+        return value;
+    }
+}
+
+public record Email(string Value)
+{
+    public static string Of(string value)
+    {
+        return value;
+    }
+}
